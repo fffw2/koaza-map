9b781298a0bd5bedccd93a398f9437adc0886da2
fffw2 <78269916+fffw2@users.noreply.github.com>
2023-11-13 00:38:25 +0900

    凡例から大字リストを削除する
    凡例の大字をクリックして当該大字に移動できるわけでもないので、大字リストはあまり意味がない。
    大字を知りたければポップアップで十分。凡例から大字リストを削除する。
    これに伴い、不要になる凡例用の画像もすべて削除する。

--- kusatsu/layers/layers.js
+++ kusatsu/layers/layers.js
@@ -72,45 +72,7 @@ var lyr_kusatsu_city_koaza_map_5 = new ol.layer.Vector({
                 source:jsonSource_kusatsu_city_koaza_map_5, 
                 style: style_kusatsu_city_koaza_map_5,
                 interactive: true,
-    title: 'kusatsu_city_koaza_map<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_0.png" /> 芦浦町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_1.png" /> 岡本町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_2.png" /> 下笠町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_3.png" /> 下寺町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_4.png" /> 下物町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_5.png" /> 橋岡町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_6.png" /> 駒井沢町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_7.png" /> 穴村町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_8.png" /> 御倉町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_9.png" /> 山寺町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_10.png" /> 山田町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_11.png" /> 志那中町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_12.png" /> 志那町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_13.png" /> 集町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_14.png" /> 渋川町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_15.png" /> 上笠町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_16.png" /> 上寺町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_17.png" /> 新堂町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_18.png" /> 新浜町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_19.png" /> 青地町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_20.png" /> 川原町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_21.png" /> 草津町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_22.png" /> 大路井町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_23.png" /> 長束町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_24.png" /> 追分町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_25.png" /> 南笠町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_26.png" /> 南山田町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_27.png" /> 馬場町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_28.png" /> 平井町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_29.png" /> 片岡町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_30.png" /> 北山田町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_31.png" /> 北大萱町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_32.png" /> 木川町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_33.png" /> 野村町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_34.png" /> 野路町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_35.png" /> 矢橋町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_36.png" /> 矢倉町<br />\
-    <img src="styles/legend/kusatsu_city_koaza_map_5_37.png" /> <br />'
+                title: 'kusatsu_city_koaza_map'
         });
 
 lyr__0.setVisible(true);lyr_OpenStreetMap_1.setVisible(true);lyr__19741978_2.setVisible(true);lyr__19611969_3.setVisible(true);lyr__2008_4.setVisible(true);lyr_kusatsu_city_koaza_map_5.setVisible(true);
