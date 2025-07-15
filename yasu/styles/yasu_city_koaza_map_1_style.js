382d9e8068092572ff17c33dbec7d8d646d4e065
fffw2 <78269916+fffw2@users.noreply.github.com>
2025-07-16 02:26:20 +0900

    ズームレベルに応じてラベルのフォントサイズを変化させる

--- yasu/styles/yasu_city_koaza_map_1_style.js
+++ yasu/styles/yasu_city_koaza_map_1_style.js
@@ -195,18 +195,24 @@ var style_yasu_city_koaza_map_1 = function(feature, resolution){
     };
     var value = feature.get("大字");
     var labelText = "";
-    size = 0;
-    var labelFont = "13.0px \'Hiragino Maru Gothic Pro\', sans-serif";
-    var labelFill = "#323232";
-    var bufferColor = "#fafafa";
-    var bufferWidth = 1.0;
-    var textAlign = "left";
-    var offsetX = 8;
-    var offsetY = 3;
-    var placement = 'point';
     if (feature.get("小字") !== null) {
         labelText = String(feature.get("小字"));
     }
+    size = 0;
+    var zoom = map.getView().getZoom();
+    var fontSize;
+    if (zoom >= 12 && zoom < 14.6) {
+        labelText = "";
+    } else if (zoom >= 14.6 && zoom < 16) {
+        fontSize = zoom * 5 - 67; // 6〜13
+    } else if (zoom >= 16 && zoom <= 18) {
+        fontSize = 13;
+    }
+    var labelFont = fontSize + "px \'Hiragino Maru Gothic Pro\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 3.0;
+    var placement = 'point';
     
 var style = categories_yasu_city_koaza_map_1(feature, value, size, resolution, labelText,
                           labelFont, labelFill, bufferColor,
